7f6e2b502808c4d9c2f2e1ac436defade974142e
oxKNYxo <kkny950@gmail.com>
2024-12-22 00:54:17 +0800

    Update script.js

--- jianwan_barcode/script.js
+++ jianwan_barcode/script.js
@@ -346,7 +346,7 @@ function updateTotal() {
 // 獲取攝像頭設備清單
 async function getCameras() {
     const devices = await navigator.mediaDevices.enumerateDevices();
-    alert('A');
-    alert(devices.filter(device => device.kind === 'videoinput')[0].label);
+    // alert('A');
+    // alert(devices.filter(device => device.kind === 'videoinput')[0].label);
     return devices.filter(device => device.kind === 'videoinput');
 }
